d59cc92d8eb29fad3f4fc4e4eb6e8943f83e6f12
sagekoala <brandondukescodes@gmail.com>
2023-09-02 08:53:26 -0500

    Add event listener to previous button
    Added event listener to previous button which will enable user
    to click through the gallery and view everything through the main
    image section of the page.

--- js/image-selection.js
+++ js/image-selection.js
@@ -1,13 +1,43 @@
 // JS for project-pages
 
 // Store reference to main image, store array of imgs with id="small" in arrSmallImages
-const mainPic = document.querySelector("#main-image");
+const mainImage = document.querySelector("#main-image");
 const arrSmallImages = document.querySelectorAll("#small-image");
 
 // Loop through arrSmallPics, set event listener on each image, replace main img with
 // selected img
 arrSmallImages.forEach((image) => {
     image.addEventListener('click', (e) => {
-        mainPic.src = `${e.srcElement.src}`
+        mainImage.src = `${e.srcElement.src}`
     });
 });
+
+// Reference to next and previous buttons
+const previousBtn = document.querySelector(".previous-btn");
+const nextBtn = document.querySelector(".next-btn");
+
+function selectPreviousImage() {
+
+    let newKey= 0;
+
+    // Get current main image and key
+    const currentMainImage = document.querySelector("#main-image");
+    const currentKey = currentMainImage.getAttribute("data-key");
+    const intCurrentKey = parseInt(currentKey);
+    
+    if ((intCurrentKey - 1) < 0) {
+        newKey = (arrSmallImages.length - 1);
+    } else {
+        newKey = parseInt(intCurrentKey) - 1;
+    }
+
+    // Find image with data-key=newKey, update main image 
+    const newCurrentImage = document.querySelector(`img[data-key="${newKey}"]`);
+    currentMainImage.src = newCurrentImage.src;
+    currentMainImage.setAttribute('data-key', newKey.toString());
+
+}
+
+
+// Add event listener on buttons
+previousBtn.addEventListener('click', selectPreviousImage);
