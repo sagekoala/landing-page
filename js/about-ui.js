97492b8a432fb8b938b1fd63b6554e7dc6969863
sagekoala <brandondukescodes@gmail.com>
2023-09-25 18:08:43 -0500

    Refactor and comment
    Added an eventHandler() function and a displaySelectedElement()
    function. The displaySelectedElement() function sets display to
    'block' for the corresponding selected element and sets display to none
    for sibling elements. Similarly, the displaySelectedElement() function
    adds the active class to the selected element and removes it from
    sibling elements. Deleted reference to content section in favor
    of dynamic reference of current selected element within for
    displaySelectedElement() function.

--- js/about-ui.js
+++ js/about-ui.js
@@ -1,49 +1,36 @@
 // Script to control sidebar selectors and display corresponding sections
 
-// Reference to content sections
-const introductionSection = document.querySelector('.introduction-section');
-const servicesSection = document.querySelector('.services-section');
-const certificationsSection = document.querySelector('.certifications-section');
-
 // List of sidebar selectors
 const selectors = document.querySelectorAll('.selector');
 
-// Set up event listener on each selector, display corresponding text
+// Rewrite below eventListener but create eventHandler function
 selectors.forEach((selector) => {
-    selector.addEventListener('click', (e) => {
-        let selection = e.srcElement.innerText;
-        let kids = e.srcElement.parentElement.children;
-
-        console.log(introductionSection, introductionSection);
-
-        if (selection === 'Introduction') {
-            introductionSection.style.display = 'block';
-            servicesSection.style.display = 'none';
-            certificationsSection.style.display = 'none';
-
-        } else if (selection === 'Services') {
-            servicesSection.style.display = 'block';
-            introductionSection.style.display = 'none';
-            certificationsSection.style.display = 'none';
-        } else if (selection === 'Certifications') {
-            certificationsSection.style.display = 'block';
-            introductionSection.style.display = 'none';
-            servicesSection.style.display = 'none';
-        }
-
-        for (let i = 0; i < kids.length; i++) {
-            if (kids[i].innerText !== selection) {
-                kids[i].classList.remove('active');
-
-                let temp = kids[i].innerText.toLowerCase();
-
-                console.log(`${temp}`);
-            } else {
-                console.log(kids[i].classList.add('active'));
-            }
-        }
-    });
+    selector.addEventListener('click', eventHandler);
 });
 
+// Pass the event to displaySelectedElement();
+function eventHandler(selectedElement) {
+    displaySelectedElement(selectedElement);
+}
 
+// Add active class and display content for selectedElement, remove display for non-selected siblings
+function displaySelectedElement(selectedElement) {
+    let kids = selectedElement.srcElement.parentElement.children; // Node list of selectedElement and siblings
+    let selectedElementText = selectedElement.srcElement.innerText;
 
+    for (let i = 0; i < kids.length; i++) {
+
+        // Get reference to currentElement
+        let temp = kids[i].innerText.toLowerCase() + '-section';
+        let currentElement = document.querySelector(`.${temp}`);
+
+        // If not the selected element, remove active class and set display to none
+        if (kids[i].innerText !== selectedElementText) {
+            kids[i].classList.remove('active');
+            currentElement.style.display = 'none';
+        } else {
+            kids[i].classList.add('active');
+            currentElement.style.display = 'block';
+        }
+    }
+}
