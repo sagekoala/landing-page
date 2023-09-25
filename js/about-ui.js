e7eb73e4f4cbe8ed80d92647ac2e9c5331dc8b42
sagekoala <brandondukescodes@gmail.com>
2023-09-25 17:31:08 -0500

    Script to add and remove active class to selectors onClick
    The script sets an event listener on each of the selectors in the
    sidebar, adds the active class to sile the user selected selector,
    and removes the active class from all other selectors.

--- js/about-ui.js
+++ js/about-ui.js
@@ -3,16 +3,19 @@
 // Reference to content sections
 const introductionSection = document.querySelector('.introduction-section');
 const servicesSection = document.querySelector('.services-section');
-const certificationsSection = document.querySelector('.certification-section');
+const certificationsSection = document.querySelector('.certifications-section');
 
 // List of sidebar selectors
 const selectors = document.querySelectorAll('.selector');
 
 // Set up event listener on each selector, display corresponding text
-
 selectors.forEach((selector) => {
     selector.addEventListener('click', (e) => {
         let selection = e.srcElement.innerText;
+        let kids = e.srcElement.parentElement.children;
+
+        console.log(introductionSection, introductionSection);
+
         if (selection === 'Introduction') {
             introductionSection.style.display = 'block';
             servicesSection.style.display = 'none';
@@ -26,6 +29,18 @@ selectors.forEach((selector) => {
             certificationsSection.style.display = 'block';
             introductionSection.style.display = 'none';
             servicesSection.style.display = 'none';
+        }
+
+        for (let i = 0; i < kids.length; i++) {
+            if (kids[i].innerText !== selection) {
+                kids[i].classList.remove('active');
+
+                let temp = kids[i].innerText.toLowerCase();
+
+                console.log(`${temp}`);
+            } else {
+                console.log(kids[i].classList.add('active'));
+            }
         }
     });
 });
